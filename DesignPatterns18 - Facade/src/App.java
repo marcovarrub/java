d77f539a0963ef1b8ef59f7d38b9f5ab30ccae11
Marco Covarrubias <marcovarrub@gmail.com>
2017-02-26 18:02:08 -0800

    Design Patterns

---
+++ DesignPatterns18 - Facade/src/App.java
@@ -0,0 +1,15 @@
+public class App {
+	public static void main(String[] args) {
+		
+		Game game = new Game();
+		
+		// Game loop
+		while(true) {
+			game.update();
+		}
+	}
+}
+
+// www.caveofprogramming.com
+
+
